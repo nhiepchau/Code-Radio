34dd7085633912eece76935c8d5169a23524cd8f
unknown <hung406474@gmail.com>
2022-01-24 00:17:24 +0700

    todo-list logic

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,3 +1,42 @@
 'use-strict'
 
-console.log('This is toDoList.js');
+const navBtn = document.querySelector('.nav #left-item .toggle-btn')
+const todoList = document.querySelector('.toDoList')
+const taskInput = document.querySelector('.toDoList .add-to-do')
+const taskList = document.querySelector('.toDoList .task-list')
+
+document.addEventListener('keypress',(e) => {
+    if(e.key == 'Enter' && taskInput.value!='') addTask()
+})
+
+navBtn.addEventListener('click',(e) => {
+    todoList.classList.toggle('invisible')
+})
+
+todoList.addEventListener('click',(e) => {
+    if(e.target.classList.contains('check-item'))
+        checkTask(e.target);
+    else if(e.target.classList.contains('remove-item'))
+        removeTask(e.target);
+})
+
+function addTask() {
+    let input = taskInput.value
+    let html = `<li class="task-item">
+    <i class="far fa-square check-item"></i>
+    <p class="task-text">${input}</p>
+    <i class="fas fa-times remove-item"></i>
+    </li>`
+    taskList.insertAdjacentHTML('beforeend', html)
+    taskInput.value = ''
+}
+
+function checkTask(checkIcon) {
+    checkIcon.classList.toggle('fa-square')
+    checkIcon.classList.toggle('fa-check-square')
+    checkIcon.nextElementSibling.classList.toggle('task-text-checked')
+}
+
+function removeTask(removeIcon) {
+    removeIcon.parentNode.remove()
+}
